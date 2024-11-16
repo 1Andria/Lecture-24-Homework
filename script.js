67fc89cc322e50ae377c1bba9484283e1b851210
1Andria <andriamelua22@gmail.com>
2024-11-16 19:26:03 +0400

    updated

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+//დღეები
 let days = parseInt(prompt("შეიყვანეთ დღე"));
+while (isNaN(days)) {
+  days = prompt("გთხოვთ დღის მაჩვენებელში შეიყვანოთ რიცხვი.");
+}
+while (days < 0) {
+  days = prompt("დღის მაჩვენებელი ვერ იქნება 0ზე ნაკლები");
+}
+
+//საათები
 let hours = parseInt(prompt("შეიყვანეთ საათი"));
+while (isNaN(hours)) {
+  hours = prompt("გთხოვთ საათის მაჩვენებელში შეიყვანოთ რიცხვი.");
+}
+while (hours > 24 || hours < 0) {
+  hours = prompt(
+    "გთხოვთ საათის მაჩვენებელში შეიყვანოთ რელევანტური რიცხვი(0-დან 23-ის ჩათვლით)"
+  );
+}
+
+//წუთები
 let minutes = parseInt(prompt("შეიყვანეთ წუთი"));
+while (isNaN(minutes)) {
+  minutes = prompt("გთხოვთ წუთების მაჩვენებელში შეიყვანოთ რიცხვი.");
+}
+while (minutes > 60 || minutes < 0) {
+  minutes = prompt(
+    "გთხოვთ წუთების მაჩვენებელში შეიყვანოთ რელევანტური რიცხვი(0-დან 59-ის ჩათვლით)"
+  );
+}
+
+//წამები
 let seconds = parseInt(prompt("შეიყვანეთ წამი"));
+while (isNaN(seconds)) {
+  seconds = prompt("გთხოვთ წამების მაჩვენებელში შეიყვანოთ რიცხვი.");
+}
+while (seconds > 60 || seconds < 0) {
+  seconds = prompt(
+    "გთხოვთ წამების მაჩვენებელში შეიყვანოთ რელევანტური რიცხვი(0-დან 59-ის ჩათვლით)"
+  );
+}
 
 let day = document.getElementById("day");
 let hour = document.getElementById("hour");
@@ -12,44 +49,6 @@ day.textContent = days;
 hour.textContent = hours;
 minute.textContent = minutes;
 second.textContent = seconds;
-
-//დღეების გასწორება
-while (isNaN(days)) {
-  days = prompt("გთხოვთ დღის მაჩვენებელში შეიყვანოთ რიცხვი.");
-}
-while (days < 0) {
-  days = prompt("დღის მაჩვენებელი ვერ იქნება 0ზე ნაკლები");
-}
-
-//საათების გასწორება
-while (isNaN(hours)) {
-  hours = prompt("გთხოვთ საათის მაჩვენებელში შეიყვანოთ რიცხვი.");
-}
-while (hours > 24 || hours < 0) {
-  hours = prompt(
-    "გთხოვთ საათი მაჩვენებელში შეიყვანოთ რელევანტური რიცხვი(0-დან 23-ის ჩათვლით)"
-  );
-}
-
-//წუთების გასწორება
-while (isNaN(minutes)) {
-  minutes = prompt("გთხოვთ წუთების მაჩვენებელში შეიყვანოთ რიცხვი.");
-}
-while (minutes > 60 || minutes < 0) {
-  minutes = prompt(
-    "გთხოვთ წუთების მაჩვენებელში შეიყვანოთ რელევანტური რიცხვი(0-დან 59-ის ჩათვლით)"
-  );
-}
-
-//წამების გასწორება
-while (isNaN(seconds)) {
-  seconds = prompt("გთხოვთ წამების მაჩვენებელში შეიყვანოთ რიცხვი.");
-}
-while (seconds > 60 || seconds < 0) {
-  seconds = prompt(
-    "გთხოვთ წამების მაჩვენებელში შეიყვანოთ რელევანტური რიცხვი(0-დან 59-ის ჩათვლით"
-  );
-}
 
 let Interval = setInterval(() => {
   if (seconds > 0) {
